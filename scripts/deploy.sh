cecf8c5452ab4da66ad37b3846241544be11d9dc
guido offermans <guidooffermans@hotmail.com>
2022-10-08 00:19:22 +0200

    deploy script update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,13 @@
+pm2 delete ~/apps/payload-cms/ecosystem.config.js
+
 rm -rf ~/apps/payload-cms/app
 
 mkdir ~/apps/payload-cms/app
 
-tar -xzf ~/apps/payload-cms/artifacts/build.tgz ~/apps/payload-cms/app
+cp ~/apps/payload-cms/artifacts/build.tgz ~/apps/payload-cms/app
 
-#rm ~/apps/payload-cms/artifacts/build.tgz
+tar -xzf ~/apps/payload-cms/app/build.tgz
+
+rm ~/apps/payload-cms/app/build.tgz
+
+pm2 start ~/apps/payload-cms/ecosystem.config.js
