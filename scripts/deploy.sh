8d9dac5950e44ef76b4575ffcf2ebefe46eca067
guido offermans <guidooffermans@hotmail.com>
2022-10-07 23:58:52 +0200

    do not remove artifact yet

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ mkdir ~/apps/payload-cms/app
 
 tar -xzf ~/apps/payload-cms/artifacts/build.tgz ~/apps/payload-cms/app
 
-rm ~/apps/payload-cms/artifacts/build.tgz
+#rm ~/apps/payload-cms/artifacts/build.tgz
