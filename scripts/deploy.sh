74e890028a878798701d9ebf187fb3296d3d8942
guido offermans <guidooffermans@hotmail.com>
2022-10-08 22:41:57 +0200

    unpack

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,6 @@ mkdir ~/apps/payload-cms/app
 
 cp ~/apps/payload-cms/artifacts/build.tgz ~/apps/payload-cms/app
 
-tar -xzf ~/apps/payload-cms/app/build.tgz && echo "DONE"
+tar -xzf ~/apps/payload-cms/app/build.tgz . && echo "DONE"
 
 sleep 10
