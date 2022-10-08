cc621c57f711c9b14b4ce35ec18bb90820b9d47e
guido offermans <guidooffermans@hotmail.com>
2022-10-08 22:29:32 +0200

    try sleep

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -ex
+
 rm -rf ~/apps/payload-cms/app
 
 mkdir ~/apps/payload-cms/app
@@ -5,3 +9,5 @@ mkdir ~/apps/payload-cms/app
 cp ~/apps/payload-cms/artifacts/build.tgz ~/apps/payload-cms/app
 
 tar -xzf ~/apps/payload-cms/app/build.tgz
+
+sleep 10
