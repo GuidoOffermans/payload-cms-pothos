dcce8b35f311ac35c8c8cef72393bbe25a00459a
guido offermans <guidooffermans@hotmail.com>
2022-10-08 22:08:35 +0200

    simplify script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,3 @@ mkdir ~/apps/payload-cms/app
 cp ~/apps/payload-cms/artifacts/build.tgz ~/apps/payload-cms/app
 
 tar -xzf ~/apps/payload-cms/app/build.tgz
-
-rm ~/apps/payload-cms/app/build.tgz
-
-# pm2
